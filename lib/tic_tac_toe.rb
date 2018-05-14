a0527398f08f85a447e6ff015a48ada3972e2e1c
Elaine Anderson <eparie@bu.edu>
2018-05-14 14:59:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ end
 end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && position_taken?(board,index) == false
+  if index.between?(0,8) && !position_taken?(board,index) 
     true
   else
     nil
